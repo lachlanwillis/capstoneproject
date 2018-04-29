3110bc966e422823120dc7f6456cae340aca486b
Bennett Hardwick <bennetthardwick@icloud.com>
2018-04-29 12:55:30 +1000

    How to create different components in Angular

--- apps/rubbish/src/app/app.module.ts
+++ apps/rubbish/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { ExampleComponent } from './example/example.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'example', component: ExampleComponent }
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExampleComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
