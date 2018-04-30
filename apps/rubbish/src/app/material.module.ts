20a610f0dfc3ef45868128e1d4708b5e98736622
Lachlan Willis <lachlanwillis12@gmail.com>
2018-04-30 12:49:09 +1000

    Created footer, added content to home, edited nav.

--- apps/rubbish/src/app/material.module.ts
+++ apps/rubbish/src/app/material.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 let materialModules: any[] = [
   MatMenuModule,
   MatButtonModule,
   MatToolbarModule,
+  MatIconModule,
 ]
 
 @NgModule({
