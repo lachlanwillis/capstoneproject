d007e6cb0922719fb4342e6b639978e9e4e0661b
Jonas Schulz <jonas.a.schulz@gmail.com>
2018-04-30 20:32:06 +1000

    #27 - added textboxes for description and title, which are tehn passed using the same post.

--- apps/rubbish/src/app/user-upload/user-upload.component.ts
+++ apps/rubbish/src/app/user-upload/user-upload.component.ts
@@ -9,16 +9,26 @@ import { HttpClient } from '@angular/common/http';
 export class UserUploadComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
-
+  currentTitle: string = null;
+  currentDescription = null;
   currentFile = null;
 
   onFileSelected(event) {
     this.currentFile = event.target.files[0];
   }
 
+  changeTitleValue(event) {
+    this.currentTitle = event.target.value;
+  }
+
+  changeDescriptionValue(event) {
+    this.currentDescription = event.target.value;
+  }
+
   onUploadFile() {
     const fd = new FormData();
-    fd.append('image', this.currentFile, this.currentFile.name);
+    fd.append(this.currentTitle, this.currentFile, this.currentDescription);
+    
     this.http.post('/api/upload-image', fd).subscribe(res => {
       console.log(res);
     });
