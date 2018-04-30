ad31931ddd4fbaec30c3261359165145ea88c1e0
Lachlan Willis <lachlanwillis12@gmail.com>
2018-04-30 15:32:48 +1000

    Removed dependencies in package.json and moved variables in footer.component.ts to top for consistency.

--- apps/rubbish/src/app/footer/footer.component.ts
+++ apps/rubbish/src/app/footer/footer.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  today = Date.now();
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  today = Date.now();
 
 }
