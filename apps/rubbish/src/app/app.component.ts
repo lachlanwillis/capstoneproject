efce7cd99c7550e60ba3f1ed0b3a85941f2b5bb1
Lachlan Willis <lachlanwillis12@gmail.com>
2018-05-01 17:49:25 +1000

    Added user portal component, updated upload to material.

--- apps/rubbish/src/app/app.component.ts
+++ apps/rubbish/src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router, RoutesRecognized } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+
+  title: string;
+
+  constructor(
+    private readonly route: ActivatedRoute, 
+    private readonly router: Router
+  ) { 
+    this.router.events.subscribe(data =>{
+      if (data instanceof RoutesRecognized) {
+        this.title = data.state.root.firstChild.data.title;
+       }
+    });
+  }
+}
