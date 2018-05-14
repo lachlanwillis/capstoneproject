932d05a418a46135cd8f5cab478c9dcf321d3794
Bennett Hardwick <bennetthardwick@icloud.com>
2018-05-14 22:02:48 +1000

    Fixed the cookie issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { Application } from 'express';
 import { connect } from 'mongoose';
 
 import { Router } from './router';
+import { authentication } from './authentication';
 
 // Connect to the database. This shouldn't be here. 
 // In future when we deploy we should get rid of this,
@@ -15,14 +16,14 @@ connect('mongodb://localhost/rubbish');
 export const app: Application = express();
 
 app.use(bodyParser.json());
-
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
-}))
-
+  cookie: { secure: false }
+}));
+app.use(authentication.initialize());
+app.use(authentication.session());
 app.use(Router);
 
 app.get('/*', (req, res) => res.send('Hey!'));
