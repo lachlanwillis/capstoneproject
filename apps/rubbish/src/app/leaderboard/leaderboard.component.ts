fc8c0ae6b1758b2e0d1e9480899db384e19d1129
Jonas Schulz <jonas.a.schulz@gmail.com>
2018-08-25 22:55:22 +1000

    First implemenation of the leaderboard - tabs for the different leaderboards

---
+++ apps/rubbish/src/app/leaderboard/leaderboard.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource, MatDialog, MatOption } from '@angular/material';
+import { HttpClient } from '@angular/common/http';
+
+export interface States {
+  value: string;
+  viewValue: string;
+}
+
+
+@Component({
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.component.html',
+  styleUrls: ['./leaderboard.component.css']
+})
+export class LeaderboardComponent implements OnInit {
+  
+  states: States[] = [
+    { value: 'act', viewValue: 'Australian Capital Territory' },
+    { value: 'nsw', viewValue: 'New South Wales' },
+    { value: 'nt', viewValue: 'Northern Territory' },
+    { value: 'qld', viewValue: 'Queensland' },
+    { value: 'sa', viewValue: 'South Australia' },
+    { value: 'tas', viewValue: 'Tasmania' },
+    { value: 'vic', viewValue: 'Victoria' },
+    { value: 'wa', viewValue: 'Western Australia' },
+  ]
+
+
+  leaderboardDisplayColumns = ['name', 'points'];
+  leaderboardData = [];
+  leaderboardSource = new MatTableDataSource<Element>(this.leaderboardData);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
