54a8d509c245d0c03372c25f42e2f70f54962903
Jonas Schulz <jonas.a.schulz@gmail.com>
2018-08-26 11:21:15 +1000

    Added Leaderboards and fetching of values

--- apps/rubbish/src/app/leaderboard/leaderboard.component.ts
+++ apps/rubbish/src/app/leaderboard/leaderboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource, MatDialog, MatOption } from '@angular/material';
 import { HttpClient } from '@angular/common/http';
+import { take, map } from 'rxjs/operators';
+
+const NUM_IMAGES = 100;
 
 export interface States {
   value: string;
@@ -14,6 +17,7 @@ export interface States {
   styleUrls: ['./leaderboard.component.css']
 })
 export class LeaderboardComponent implements OnInit {
+  postcodeText: any;
   
   states: States[] = [
     { value: 'act', viewValue: 'Australian Capital Territory' },
@@ -31,12 +35,60 @@ export class LeaderboardComponent implements OnInit {
   leaderboardData = [];
   leaderboardSource = new MatTableDataSource<Element>(this.leaderboardData);
 
+  leaderboardStateData = [];
+  leaderboardStateSource = new MatTableDataSource<Element>(this.leaderboardStateData);
+
+  leaderboardPostcodeData = [];
+  leaderboardPostcodeSource = new MatTableDataSource<Element>(this.leaderboardPostcodeData);
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    this.fetchNational();
   }
+  // Getting National Leaderboard
+  fetchNational() {
+    this.getNationalLeaderboard().subscribe((ladder: any[]) =>
+      this.leaderboardSource = new MatTableDataSource<any>(ladder.map(i => { return { name: i.name, points: i.points } }))
+    );
+  }
+
+  // Getting State Leaderboard
+  fetchState(event: any) {
+    const state = event.value;
+    this.getStateLeaderboard(state).subscribe((ladder: any[]) =>
+      this.leaderboardStateSource = new MatTableDataSource<any>(ladder.map(i => { return { name: i.name, points: i.points } }))
+    );
+  }
+
+  // Getting Postcode Leaderboard
+  fetchPostcode() {
+    
+    const postcode = this.postcodeText;
+    this.getPostCodeLeaderboard(postcode).subscribe((ladder: any[]) =>
+      this.leaderboardPostcodeSource = new MatTableDataSource<any>(ladder.map(i => { return { name: i.name, points: i.points } }))
+    );
+  }
+
+
+
+  getPostCodeLeaderboard(postcode: string) {
+    return this.http.get<any[]>(`/api/leaderboard/${postcode}/${NUM_IMAGES}`).pipe(take(1));
+  }
+
+  getStateLeaderboard(state: string) {
+    return this.http.get<any[]>(`/api/leaderboard/${state}/${NUM_IMAGES}`).pipe(take(1));
+    
+  }
+
+  getNationalLeaderboard() {
+    return this.http.get<any[]>(`/api/leaderboard/all/${NUM_IMAGES}`).pipe(take(1));
+  }
+
+
 
 }
