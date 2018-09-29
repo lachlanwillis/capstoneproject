53521b16834cfedcc3680600ac7234f165363e96
Jonas Schulz <jonas.a.schulz@gmail.com>
2018-09-29 18:34:57 +1000

    Profile shows rank and points of user under stats - current equation for determining rank is - sqrt(points/10)+1. So 10 points to lvl 2, 40 to lvl 3, 90 to lvl 4, etc.

--- apps/rubbish/src/app/account-stats/account-stats.component.ts
+++ apps/rubbish/src/app/account-stats/account-stats.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { ImageService } from '../images/image.service';
 import { Image } from '../image/image';
+import { AuthService } from '../auth/auth.service';
+
 
 @Component({
   selector: 'app-account-stats',
@@ -10,11 +12,15 @@ import { Image } from '../image/image';
 export class AccountStatsComponent implements OnInit {
 
   images: Image[] = [];
+  user?: any;
   totalImages: number;
   totalDetections: number;
+  currentRank: number;
+ 
 
   constructor(
-    private readonly image: ImageService
+    private readonly image: ImageService,
+    private readonly auth: AuthService
   ) {}
 
   ngOnInit() {
@@ -23,7 +29,21 @@ export class AccountStatsComponent implements OnInit {
          this.images = res;
          this.totalImages = res.length;
          this.totalDetections = this.images.reduce((acc, image) => (acc + image.detections.length), 0);
-       });
+      });
+
+    this.auth.getCurrentUser()
+      .subscribe(user => {
+        this.user = user;
+
+        if (this.user.points < 10) {
+          this.currentRank = 1
+        } else {
+          this.currentRank = Math.floor(Math.pow(parseFloat(this.user.points) / 10.0, 0.5)) + 1;
+        }
+      });
+
+    
+    
   }
 
 }
