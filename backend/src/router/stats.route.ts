873ba7a6825e6530988a62bc3511aa55dce4249d
Bennett Hardwick <me@bennetthardwick.com>
2018-10-16 00:13:32 +1000

    Add contributors

--- backend/src/router/stats.route.ts
+++ backend/src/router/stats.route.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "express";
-import { Image } from "../models";
+import { Image, User } from "../models";
 
 export const RubbishDistributionHandler: RequestHandler = (req, res) => {
   Image.find({})
@@ -17,3 +17,9 @@ export const RubbishDistributionHandler: RequestHandler = (req, res) => {
     .then(result => res.json(result))
     .catch(err => res.status(500).json({ error: true, message: err.message }));
 };
+
+export const ContributersStats: RequestHandler = (req, res) => {
+  User.find({})
+    .then(users => res.json({ users: users.length }))
+    .catch(e => res.status(500).json({ error: true, message: e.message }));
+}
